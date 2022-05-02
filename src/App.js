01d8b7faee53ce498e6fa92c89a6ec32724c497b
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-05-03 06:33:52 +0700

    upload code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header/Header';
+import Content from './Content/Content';
+import Slider from './Content/Slider';
+import Promotion from './Content/Promotion';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Slider></Slider>
+      <Promotion></Promotion>
+      <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
